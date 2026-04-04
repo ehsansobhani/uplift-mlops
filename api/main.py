ce0454c498930321ae17e550fc24e2b26e1317fb
ES <E.sobhani2000@gmail.com>
2026-04-04 10:44:44 -0400

    update CI/CD pipeline

--- api/main.py
+++ api/main.py
@@ -1,3 +1,5 @@
+
+import logging
 from fastapi import FastAPI
 from pydantic import BaseModel
 from src.predict import predict_uplift
@@ -10,7 +12,20 @@ app = FastAPI()
 #     feature3: float
 class Req(BaseModel):
     client_id: str
-    
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# @app.post("/predict")
+# def p(r: Req):
+#     return {"uplift": predict_uplift(r.client_id)}
+
 @app.post("/predict")
 def p(r: Req):
-    return {"uplift": predict_uplift(r.client_id)}
+    logger.info(f"Request received: {r.client_id}")
+
+    uplift = predict_uplift(r.client_id)
+
+    logger.info(f"Prediction: {uplift}")
+
+    return {"uplift": uplift}
