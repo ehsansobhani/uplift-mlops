272ca1b61bb4cf89aad4f7fb41f8771e15b81d42
ES <E.sobhani2000@gmail.com>
2026-04-04 10:50:45 -0400

    updated CI/CD pipeline

--- api/main.py
+++ api/main.py
@@ -4,6 +4,11 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from src.predict import predict_uplift
 
+
+from prometheus_client import Counter, Histogram, generate_latest
+from fastapi.responses import Response
+import time
+
 app = FastAPI()
 
 # class Req(BaseModel):
@@ -29,3 +34,23 @@ def p(r: Req):
     logger.info(f"Prediction: {uplift}")
 
     return {"uplift": uplift}
+
+
+REQUEST_COUNT = Counter("request_count", "Total API Requests")
+REQUEST_LATENCY = Histogram("request_latency_seconds", "Request latency")
+
+@app.middleware("http")
+async def monitor_requests(request, call_next):
+    start = time.time()
+    REQUEST_COUNT.inc()
+
+    response = await call_next(request)
+
+    latency = time.time() - start
+    REQUEST_LATENCY.observe(latency)
+
+    return response
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type="text/plain")
